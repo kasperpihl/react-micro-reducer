6942b6d803845e4f02d3f88c800201d84f58bdc4
Kasper Pihl Tornøe <kasper@pihl.it>
2019-11-23 20:09:01 +0100

    master: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# useActionReducer
+
+A small extension for useReducer to make static actions
+
+## Installation
+
+```js
+npm i use-action-reducer
+```
+
+## Usage
+
+```js
+import useActionReducer from "use-action-reducer";
+```
---
+++ index.ts
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+
+// utility type constructor - remove first element of the array/tuple
+type Tail<T extends any[]> = ((...args: T) => void) extends (
+  head: any,
+  ...tail: infer U
+) => any
+  ? U
+  : never;
+
+// I removed second generic, not needed at this level
+type TMicroReducers<State> = {
+  [key: string]: (state: State, ...args: any[]) => State;
+};
+
+// introduced generic R to allow proper infering
+export default function useActionReducer<
+  State,
+  R extends TMicroReducers<State>
+>(actionReducers: R, initialState?: State) {
+  const [state, _dispatch] = useReducer(
+    (
+      state: State,
+      action: { type: keyof typeof actionReducers; payload: any[] }
+    ) => actionReducers[action.type](state, ...action.payload),
+    initialState
+  );
+
+  const dispatch: {
+    [Key in keyof R]: (...x: Tail<Parameters<R[Key]>>) => void;
+  } = {} as any;
+
+  for (let key in actionReducers) {
+    const reducer = actionReducers[key];
+    dispatch[key] = (...args: Parameters<typeof reducer>[1]) =>
+      _dispatch({ type: key, payload: args });
+  }
+
+  return [state, dispatch] as [State, typeof dispatch];
+}
