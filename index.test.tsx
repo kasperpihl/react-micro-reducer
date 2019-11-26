0599478078f48c88934beea4d0c15d58ac8ccc0b
Kasper Pihl Tornøe <kasper@pihl.it>
2019-11-26 10:49:33 +0100

    master: updated tests

--- index.test.tsx
+++ index.test.tsx
@@ -1,92 +1,79 @@
-import React from "react";
-import { useMicroReducer } from "./index";
+import { useMicroReducer, TProduceFunc } from "./index";
 import produce from "immer";
-import { create, act } from "react-test-renderer";
+import { renderHook, act } from "@testing-library/react-hooks";
 
-function Tester({ reducer, initialState, producer }) {
-  const [state, dispatch] = useMicroReducer(reducer, initialState, producer);
-  function ResultInspect({ state, dispatch }) {
-    return null;
-  }
+const defaultReducer = {
+  increment: state => state + 1,
+  decrement: state => state - 1,
+  set: (state, value) => value,
+  multiplySeed: (state, a, b) => a * b
+};
+const defaultInitialState = 0;
 
-  return <ResultInspect state={state} dispatch={dispatch} />;
-}
+const setupHook = (
+  reducer,
+  initialState = undefined,
+  producer: TProduceFunc = undefined
+) =>
+  renderHook(() => {
+    const [state, dispatch] = useMicroReducer(reducer, initialState, producer);
+    return {
+      state,
+      dispatch
+    };
+  });
 
-function createTester(
-  reducer: Parameters<typeof useMicroReducer>[0],
-  initialState?: Parameters<typeof useMicroReducer>[1],
-  producer?: Parameters<typeof useMicroReducer>[2]
-) {
-  const testRenderer = create(
-    <Tester reducer={reducer} initialState={initialState} producer={producer} />
-  );
-
-  return {
-    getState: () => testRenderer.toTree().rendered.props.state,
-    dispatch: testRenderer.toTree().rendered.props.dispatch
-  };
-}
-
-const getDefaultTester = () =>
-  createTester(
-    {
-      increment: state => state + 1,
-      decrement: state => state - 1,
-      set: (state, value) => value,
-      multiplySeed: (state, a, b) => a * b
-    },
-    0
-  );
+const setupDefaultHook = () => setupHook(defaultReducer, defaultInitialState);
 
 test("Initial state: undefined", () => {
-  const { getState } = createTester({});
-  expect(getState()).toBe(undefined);
+  const { result } = setupHook({}, undefined);
+  expect(result.current.state).toBe(undefined);
 });
 
 test("Initial state: 0", () => {
-  const { getState } = getDefaultTester();
-  expect(getState()).toBe(0);
+  const { result } = setupDefaultHook();
+  expect(result.current.state).toBe(0);
 });
 
 test("Dispatch gets the expected actions", () => {
   const expectedActions = ["decrement", "increment", "multiplySeed", "set"];
-  const { dispatch } = getDefaultTester();
-  const actualActions = Object.keys(dispatch);
+  const { result } = setupDefaultHook();
+  const actualActions = Object.keys(result.current.dispatch);
   actualActions.sort();
 
   expect(actualActions).toEqual(expectedActions);
 });
 
 test("Dispatch with NO arguments work", () => {
-  const { getState, dispatch } = getDefaultTester();
-  const currentState = getState();
+  const { result } = setupDefaultHook();
+  const currentState = result.current.state;
   act(() => {
-    dispatch.increment();
+    result.current.dispatch.increment();
   });
-  expect(getState()).toBe(currentState + 1);
+  expect(result.current.state).toBe(currentState + 1);
 });
 
 test("Dispatch with ONE arguments work", () => {
-  const { getState, dispatch } = getDefaultTester();
+  const { result } = setupDefaultHook();
 
   act(() => {
-    dispatch.set(42);
+    result.current.dispatch.set(42);
   });
-  expect(getState()).toBe(42);
+  expect(result.current.state).toBe(42);
 });
 
 test("Dispatch with MULTIPLE arguments work", () => {
-  const { getState, dispatch } = getDefaultTester();
+  const { result } = setupDefaultHook();
 
   act(() => {
-    dispatch.multiplySeed(7, 6);
+    result.current.dispatch.multiplySeed(7, 6);
   });
-  expect(getState()).toBe(42);
+  expect(result.current.state).toBe(42);
 });
 
 // Immer tests
-const getImmerTester = () =>
-  createTester(
+const setupImmerHook = () =>
+  setupHook(
     {
       search: (draft, query1: string, query2: string) => {
         draft.query = query1 + " " + query2;
@@ -99,21 +86,21 @@ const getImmerTester = () =>
   );
 
 test("Immer initial state", () => {
-  const { getState } = getImmerTester();
-  expect(getState()).not.toEqual({
+  const { result } = setupImmerHook();
+  expect(result.current.state).not.toEqual({
     query: "hi"
   });
-  expect(getState()).toEqual({
+  expect(result.current.state).toEqual({
     query: ""
   });
 });
 
 test("Immer dispatch", () => {
-  const { getState, dispatch } = getImmerTester();
+  const { result } = setupImmerHook();
   act(() => {
-    dispatch.search("I am", "awesome");
+    result.current.dispatch.search("I am", "awesome");
   });
-  expect(getState()).toEqual({
+  expect(result.current.state).toEqual({
     query: "I am awesome"
   });
 });
